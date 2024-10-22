f40e480306aa81de34e07029bbd55c3b4a2a49c3
Alexander Plavin <alexander@plav.in>
2024-10-22 00:00:00 +0000

    support staticregex

--- ext/DataManipulationExt.jl
+++ ext/DataManipulationExt.jl
@@ -2,8 +2,23 @@ module DataManipulationExt
 
 using DataManipulation
 using DBCollections
+using DBCollections: colnames
 
 DataManipulation.uniqueonly(dbc::DBCollection) = unique(dbc) |> only
 DataManipulation.uniqueonly(f, dbc::DBCollection) = unique(f, dbc) |> only
+
+DBCollections.ix_to_select(ix::DataManipulation.StaticRegex, dbc) =
+    filter(collect(colnames(dbc))) do n
+        occursin(DataManipulation.unstatic(typeof(ix)), String(n))
+    end
+
+function DBCollections.ix_to_select(ix::Pair{<:DataManipulation.StaticRegex, <:DataManipulation.StaticSubstitution}, dbc)
+    regex, subs = DataManipulation.unstatic.(typeof.([ix[1], ix[2]]))
+    filtermap(collect(colnames(dbc))) do n
+        if occursin(regex, String(n))
+            Symbol(replace(String(n), regex => subs)) => n
+        end
+    end
+end
 
 end
