dac4e1f2c20ed29c1ceb2c94042c83bb70645c8f
Alexander Plavin <alexander@plav.in>
2024-12-16 00:00:00 +0000

    minor

--- ext/SQLiteExt.jl
+++ ext/SQLiteExt.jl
@@ -3,8 +3,6 @@ module SQLiteExt
 using SQLite
 import DBCollections: _copy_impl!
 
-function _copy_impl!(conn::SQLite.DB, rows, tblname::Symbol)
-    SQLite.load!(rows, conn, string(tblname); ifnotexists=false)
-end
+_copy_impl!(conn::SQLite.DB, rows, tblname::Symbol) = SQLite.load!(rows, conn, string(tblname); ifnotexists=false)
 
 end
