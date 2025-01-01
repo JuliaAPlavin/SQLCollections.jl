dadc9b0967b51bf1856e20ff4d7926e5ee23da5b
Alexander Plavin <alexander@plav.in>
2025-01-01 00:00:00 +0000

    support date funcs

---
+++ ext/DatesExt.jl
@@ -0,0 +1,13 @@
+module DatesExt
+
+using Dates
+import DBCollections: Fun, func_to_funsql
+
+func_to_funsql(::typeof(year), arg) = Fun.extract("YEAR", arg)
+func_to_funsql(::typeof(month), arg) = Fun.extract("MONTH", arg)
+func_to_funsql(::typeof(day), arg) = Fun.extract("DAY", arg)
+func_to_funsql(::typeof(hour), arg) = Fun.extract("HOUR", arg)
+func_to_funsql(::typeof(minute), arg) = Fun.extract("MINUTE", arg)
+func_to_funsql(::typeof(second), arg) = Fun.extract("SECOND", arg)
+
+end
